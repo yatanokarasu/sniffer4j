51b5caaec99e414355983c2bbde292a8933b53c0
yatanokarasu <sync.outerphase@gmail.com>
2017-12-30 00:17:05 +0900

    Add draft version.
    Signed-off-by: yatanokarasu <sync.outerphase@gmail.com>

---
+++ src/main/java/io/sniffer4j/Premain.java
@@ -0,0 +1,110 @@
+/*  
+ *  The MIT License (MIT)
+ *  
+ *  Copyright (c) 2017 Yusuke TAKEI.
+ *  
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *  
+ *  The above copyright notice and this permission notice shall be included in
+ *  all copies or substantial portions of the Software.
+ *  
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  THE SOFTWARE.
+ */
+package io.sniffer4j;
+
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.instrument.ClassFileTransformer;
+import java.lang.instrument.IllegalClassFormatException;
+import java.lang.instrument.Instrumentation;
+import java.security.ProtectionDomain;
+
+import javassist.CannotCompileException;
+import javassist.ClassPool;
+import javassist.CtClass;
+import javassist.CtMethod;
+import javassist.expr.ExprEditor;
+
+
+/**
+ * 
+ */
+public final class Premain {
+    
+    private static final ClassPool CLASS_POOL = ClassPool.getDefault();
+    
+    /**
+     * @param agentArguments Java Agent arguments
+     * @param instrumentation An instrumentation instance for pre-main
+     */
+    public static void premain(final String agentArguments, final Instrumentation instrumentation) {
+        instrumentation.addTransformer(new ClassFileTransformer() {
+            
+            /**
+             * @see java.lang.instrument.ClassFileTransformer#transform(java.lang.Module,
+             *      java.lang.ClassLoader, java.lang.String, java.lang.Class,
+             *      java.security.ProtectionDomain, byte[])
+             */
+            @Override
+            public byte[] transform(Module module, ClassLoader loader, String fullyClassname, Class<?> arg3, ProtectionDomain protectionDomain,
+                byte[] classfileByteSequence)
+                throws IllegalClassFormatException {
+                return this.transform(loader, fullyClassname, arg3, protectionDomain, classfileByteSequence);
+            }
+            
+            
+            /**
+             * @see java.lang.instrument.ClassFileTransformer#transform(java.lang.ClassLoader,
+             *      java.lang.String, java.lang.Class, java.security.ProtectionDomain, byte[])
+             */
+            @Override
+            public byte[] transform(ClassLoader loader, String fullyClassname, Class<?> arg3, ProtectionDomain protectionDomain, byte[] classfileByteSequence)
+                throws IllegalClassFormatException {
+                if (!accept(fullyClassname)) {
+                    return null;
+                }
+                
+                try (final InputStream byteStream = new ByteArrayInputStream(classfileByteSequence)) {
+                    CtClass ctClass = CLASS_POOL.makeClass(byteStream);
+                    CtMethod[] ctMethods = ctClass.getDeclaredMethods();
+                    
+                    final String className = ctClass.getName();
+                    
+                    for (final CtMethod aMethod : ctMethods) {
+                        final String methodName = aMethod.getName();
+                        final String varName = methodName + "_sniff_start";
+                        aMethod.addLocalVariable(varName, CtClass.longType);
+                        aMethod.insertBefore(varName + " = System.nanoTime();");
+                        aMethod.insertAfter("System.out.println(\"" + className + "#" + methodName + "()[Thread=\" + Thread.currentThread().getName() + \"]: \" + (System.nanoTime() - " + varName + "));");
+                    }
+                    
+                    return ctClass.toBytecode();
+                } catch (IOException | CannotCompileException cause) {
+                    IllegalClassFormatException e = new IllegalClassFormatException();
+                    e.initCause(cause);
+                    
+                    throw e;
+                }
+            }
+
+
+            private boolean accept(String fullyClassname) {
+                return fullyClassname.startsWith("io");
+            }
+        });
+    }
+    
+}
